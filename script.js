671ac198fa4876df1181371bdf5010187535b155
Seun Agbede <73464412+SeunAgbede@users.noreply.github.com>
2021-11-09 15:13:32 +0000

    Add files via upload
    Added error handling statements for the server responses

--- script.js
+++ script.js
@@ -7,13 +7,26 @@ quo_btn.addEventListener('click', getQuo)
 img_btn.addEventListener('click', getDog)
     
 async function getDog() {
-    const res = await fetch('https://dog.ceo/api/breeds/image/random');
-    const data = await res.json(); 
-    dog_img.innerHTML = `<img src="${data.message}">`
-}
+    // error handling to manage Server error responses
+    try {
+        const res = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await res.json(); 
+        dog_img.innerHTML = `<img src="${data.message}">`
+    } catch (err) {
+        dog_img.innerHTML = `<p>${err.message}</p>`;
+    }
+};
+
 
 async function getQuo() {
-    const res = await fetch('https://cors-everywhere.herokuapp.com/https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1');
-    const data = await res.json();
-    dog_quote.innerHTML = data[0].fact;
-}
+    // error handling to manage Server error responses
+    try {
+        const res = await fetch('https://cors-everywhere.herokuapp.com/https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=1');
+        const data = await res.json();
+        dog_quote.innerHTML = data[0].fact;
+    } catch (err){
+        dog_quote.innerHTML = `<p>${err.message}</p>`; 
+    }
+};
+
+
